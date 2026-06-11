37daa017c3054c82eea6cee77bf2c3f0c2b3108d
antoniooricoellington <antoniooricoellington@outlook.com>
2026-06-11 09:56:55 -0400

    Update video filename reference, re-encoded H.264 MP4

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -6,10 +6,10 @@ export interface Movie {
 export const movies: Movie[] = [
   {
     title: "Accroche Movie Screening (Convened By The Oyo State Ministry Of Health)",
-    videoUrl: "/videos/MINI ACC 1080P.mp4",
+    videoUrl: "/videos/Mini Acc 1080P.mp4",
   },
   {
     title: "Accroche (trailer)",
-    videoUrl: "/videos/MINI ACC 1080P.mp4",
+    videoUrl: "/videos/Mini Acc 1080P.mp4",
   },
 ]
